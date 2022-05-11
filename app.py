439f65cf6913f054caedc0ed25f2e49fdb5de6fa
MinseonKang <s_2___@naver.com>
2022-05-11 20:09:46 +0900

    app.py 수정 commit

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
+import certifi
+ca = certifi.where()
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.3dhal.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-
+# DB에서 저장된 단어 찾아서 HTML에 나타내기
 @app.route('/')
 def home():
     return render_template('/review.html')
 
+# 극장 정보 GET API
+@app.route("/main/cinema", methods=["GET"])
+def cinema():
+    cinema_list = list(db.cinema.find({}, {'_id': False}))
+    return jsonify({'cinema':cinema_list})
 
-# review 저장하기
+# review 저장하기 API
 @app.route("/review", methods=["POST"])
 def review_save():
     store_receive = request.form['store_give']
@@ -38,7 +44,7 @@ def review_save():
     return jsonify({'msg': '등록 완료!'})
 
 
-# review 삭제하기
+# review 삭제하기 API
 @app.route("/review/delete", methods=["POST"])
 def review_delete():
     num_receive = request.form['num_give']
@@ -46,7 +52,7 @@ def review_delete():
     return jsonify({'msg': '삭제 완료!'})
 
 
-# reveiw 목록 보여주기
+# reveiw 목록 보여주기 API
 @app.route("/review", methods=["GET"])
 def review_get():
     review_list = list(db.review.find({}, {'_id': False}))
@@ -54,4 +60,4 @@ def review_get():
     return jsonify({'reviews': review_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
